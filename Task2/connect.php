1c6d629a0b0adb9c483475580a937e7742323dc8
Jharana123 <86701517+Jharana123@users.noreply.github.com>
2021-07-11 22:29:20 +0530

    Add files via upload

---
+++ Task2/connect.php
@@ -0,0 +1,13 @@
+<??php
+$name=$_POST['name']
+$email=$_POST['email']
+$PhoneNumber=$_POST['phone number']
+$Year=$_POST['Year']
+$branch=$_POST['branch']
+$password=$_POST['Password ']
+$confirmpassword=$_POST['confirm password']
+
+//database connection
+
+
+?>
